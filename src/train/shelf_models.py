afbff2e56f84bb2aaaae566219c462aa2b550294
Robert Proner <robproner@gmail.com>
2026-02-03 14:26:42 -0500

    wrote functions to train shelf models.

--- src/train/shelf_models.py
+++ src/train/shelf_models.py
@@ -4,20 +4,23 @@ import numpy as np
 from sklearn.model_selection import GridSearchCV
 import os
 from pathlib import Path
+from figures.make_quantile_plots import QUANTILES
 from src.utils.files import check_hps_exist, load_hyperparameters, save_hyperparameters
-from sklearn.linear_model import QuantileRegressor
+from sklearn.linear_model import LinearRegression, QuantileRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from quantile_forest import RandomForestQuantileRegressor
-
+from statsmodels.regression.quantile_regression import QuantReg
+from statsmodels.tools import add_constant
+from sklearn.dummy import DummyRegressor
 
 def fit_linear_models(
-        quantiles: list[float], 
-        grids: dict[dict],
         X_train_full: pd.DataFrame, 
         y_train_full: pd.Series,
         X_test: pd.DataFrame,
+        quantiles: list[float],
         target_name: str,
         year: int,
+        grids: dict[dict],
         k_folds: int,
         tuning_log_path: Path
     ):
@@ -61,13 +64,13 @@ def fit_linear_models(
     return preds
 
 def fit_qgb(
-    quantiles: list[float], 
-    grid: dict[list],
-    target_name: str,
-    year: int,
     X_train_full: pd.DataFrame, 
     y_train_full: pd.Series,
     X_test: pd.DataFrame,
+    quantiles: list[float],
+    target_name: str,
+    year: int,
+    grid: dict[list],
     k_folds: int,
     tuning_log_path: Path
 ):
@@ -100,13 +103,13 @@ def fit_qgb(
     return preds
 
 def fit_qrf(
-        quantiles: list[float],
-        grid: dict[list],
-        target_name: str,
-        year: int,
         X_train_full: pd.DataFrame, 
         y_train_full: pd.Series,
         X_test: pd.DataFrame,
+        quantiles: list[float],
+        target_name: str,
+        year: int,
+        grid: dict[list],
         k_folds: int,
         tuning_log_path: Path
     ):
@@ -142,44 +145,91 @@ def fit_qrf(
     return preds
 
 def fit_qpcr(
-        quantiles: list[float],
         X_train_full: pd.DataFrame, 
         y_train_full: pd.Series,
         X_test: pd.DataFrame,
+        quantiles: list[float],
         target_idx: int, 
         year: int,
         models_dir: Path
     ):
 
+    preds_dict = {}
+
     for q in quantiles:
 
         Q = int(q*100)
 
-    study_name = f'QPCR_Q{Q}_{target_idx}_{year}'
+        study_name = f'QPCR_Q{Q}_{target_idx}_{year}'
 
-    if not os.path.exists( models_dir / f'{study_name}_bestsubset.npy'):
-        print(f'Training {study_name}...')
+        if not os.path.exists( models_dir / f'{study_name}_bestsubset.npy'):
+            print(f'Training {study_name}...')
 
-        qpcr, best_subset = fit_qpcr(
-            X=X_train_full.values, # Constant added inside function
-            y=y_train_full.values, 
-            q=q
-        )
+            qpcr, best_subset = fit_qpcr(
+                X=X_train_full.values, # Constant added inside function
+                y=y_train_full.values, 
+                q=q
+            )
 
-        np.save(models_dir / f'{study_name}_bestsubset.npy', np.array(best_subset))
+            np.save(models_dir / f'{study_name}_bestsubset.npy', np.array(best_subset))
 
-    else:
+        else:
 
-        print(f'{study_name} already exists. Loading...')
+            print(f'{study_name} already exists. Loading...')
 
-        best_subset = list(np.load(f'{MODEL_DIR}{study_name}_bestsubset.npy', allow_pickle=True).flatten())
+            best_subset = list(np.load(f'{models_dir}{study_name}_bestsubset.npy', allow_pickle=True).flatten())
 
-        X_train_subset = X_train_full.values[:, best_subset]
-        qpcr = QuantReg(y_train_full.values, add_constant(X_train_subset, has_constant='skip')).fit(q=q)
+            X_train_subset = X_train_full.values[:, best_subset]
+            qpcr = QuantReg(y_train_full.values, add_constant(X_train_subset, has_constant='skip')).fit(q=q)
 
-    # print(f"best_subset: {best_subset}")
+        # print(f"best_subset: {best_subset}")
 
-    X_test_subset = X_test.values[:, best_subset]
-    preds = qpcr.predict(add_constant(X_test_subset, has_constant='skip')).flatten()
+        X_test_subset = X_test.values[:, best_subset]
+        preds = qpcr.predict(add_constant(X_test_subset, has_constant='skip')).flatten()
 
-    all_model_preds[f'QPCR_Q{Q}'] = preds
+        preds_dict[f'QPCR_Q{Q}'] = preds
+
+def fit_ar1(X_train_full: pd.DataFrame, y_train_full: pd.DataFrame, X_test: pd.DataFrame, quantiles: list[float], target_name: str, year: int):
+
+    preds_dict = {}
+    # X_ar_1 = pd.read_parquet(ar1_x_path)
+    # X_ar_1_train = X_ar_1.loc['1961-02-01':f'{year}-12-01', f"{target_name}_t-1"]
+    # X_ar_1_test = X_ar_1.loc[f'{year+1}-01-01': f'{year+1}-12-01', f"{target_name}_t-1"]
+    # y_train_full_ar = y_train_full.loc['1961-02-01':f'{year}-12-01'] # Get rid of first date because NaN from lag
+
+    for q in quantiles: 
+        ar_1 = QuantileRegressor(quantile=q)
+        ar_1.fit(X_train_full.values.reshape(-1,1), y_train_full.values)
+        preds = ar_1.predict(X_test.values.reshape(-1,1)) # Shape (n_samples,)
+        preds_dict[f'AR1_Q{int(q*100)}'] = preds
+
+    # Train an AR(1) specifically for the mean
+    ar1_mean = LinearRegression()
+    ar1_mean.fit(X_train_full.values.reshape(-1,1), y_train_full.values)
+    mean_preds = ar1_mean.predict(X_test.values.reshape(-1,1))
+    preds_dict['AR1_Mean'] = mean_preds
+
+    return preds_dict
+
+def fit_dummy(X_train_full: pd.DataFrame, 
+              y_train_full: pd.Series,
+              X_test: pd.DataFrame,
+              quantiles: list[float]):
+    
+    preds_dict = {}
+
+    for q in quantiles:
+        Q = int(q*100)
+        naive = DummyRegressor(strategy='quantile', quantile=q)
+        # X does not matter for DummyRegressor but required for consistency
+        naive.fit(X_train_full, y_train_full)
+        preds = naive.predict(X_test)
+        preds_dict[f'Naive_Q{Q}'] = preds
+
+    # Naive Mean Regressor
+    naive_mean = DummyRegressor(strategy='mean')
+    naive_mean.fit(X_train_full, y_train_full)
+    mean_preds = naive_mean.predict(X_test)
+    preds_dict['Naive_Mean'] = mean_preds
+
+    return preds_dict
