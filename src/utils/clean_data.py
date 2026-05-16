06973dbf8e9999be5562b9a9517e67684c60a8a0
Robert Proner <72316801+rproner1@users.noreply.github.com>
2026-05-16 13:24:57 -0400

    fixed bug in remove cols

--- src/utils/clean_data.py
+++ src/utils/clean_data.py
@@ -3,6 +3,6 @@ import pandas as pd
 def remove_cols(threshold: float, df: pd.DataFrame, train_end: str):
 
     train_df = df.loc[:train_end]
-    cols_to_keep = [c for c in df.columns if (train_df[c].isna().sum() / train_df.shape[1] < threshold)]
+    cols_to_keep = [c for c in df.columns if (train_df[c].isna().sum() / train_df.shape[0] < threshold)]
     df_clean = df.loc[:, cols_to_keep]
     return df_clean
