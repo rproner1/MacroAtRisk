10d03a5a0a0ae21c9e0273313bda2cad2bd8aa84
Robert Proner <72316801+rproner1@users.noreply.github.com>
2026-05-25 14:16:26 -0400

    fixed slstmcell

--- src/train/slstm.py
+++ src/train/slstm.py
@@ -1,6 +1,8 @@
 from tensorflow.keras.layers import Layer
 import tensorflow as tf
 from tensorflow.keras.ops import log_sigmoid, maximum, sigmoid, minimum, exp, ones_like, tanh, all
+from tensorflow.keras import initializers
+from tensorflow.keras import regularizers
 
 class CustomLSTMCell(Layer):
     def __init__(self, units, **kwargs):
@@ -40,16 +42,27 @@ class CustomLSTMCell(Layer):
 
 
 class sLSTMCell(Layer):
-    def __init__(self, units, **kwargs):
+    def __init__(
+            self, 
+            units, 
+            kernel_initializer="glorot_uniform", 
+            kernel_regularizer=None,
+            **kwargs
+        ):
         super().__init__(**kwargs)
         self.units = units
         self.state_size = (self.units, self.units, self.units, self.units)
+
+        self.kernel_initializer = initializers.get(kernel_initializer)
+
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
 
     def build(self, input_shape):
         input_dim = input_shape[-1]
         # Weights for input and hidden state
         self.kernel = self.add_weight(shape=(input_dim, 4 * self.units),
-                                 initializer='glorot_uniform',
+                                 initializer=self.kernel_initializer,
+                                 regularizer=self.kernel_regularizer,
                                  name='kernel')
         self.recurrent_kernel = self.add_weight(shape=(self.units, 4 * self.units),
                                  initializer='orthogonal',
@@ -68,10 +81,11 @@ class sLSTMCell(Layer):
 
         logfplusm = log_sigmoid(f_candidate) + m_prev
 
-        if all(n_prev == 0):
-            m = i_candidate
-        else:
-            m = maximum(logfplusm, i_candidate)
+        m = tf.cond(
+            tf.reduce_all(tf.equal(n_prev, 0.0)),
+            lambda: i_candidate,
+            lambda: tf.maximum(i_candidate, logfplusm),
+        )
         
         # usual output gate and cell state candidate
         o = sigmoid(o_candidate)
@@ -86,3 +100,14 @@ class sLSTMCell(Layer):
         h = o * c / n
         
         return h, [h, c, n, m]
+    
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "kernel_initializer": self.kernel_initializer,
+                "kernel_regularizer": self.kernel_regularizer
+            }
+        )
+        return config
+    
