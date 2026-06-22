0a19f454ea4016711820d1e52eab4af16d5d8042
Robert Proner <72316801+rproner1@users.noreply.github.com>
2026-06-22 09:22:50 -0400

    delete redundant/empty file

--- src/utils/clean_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def remove_cols(threshold: float, df: pd.DataFrame, train_end: str):
-
-    train_df = df.loc[:train_end]
-    cols_to_keep = [c for c in df.columns if (train_df[c].isna().sum() / train_df.shape[0] < threshold)]
-    df_clean = df.loc[:, cols_to_keep]
-    return df_clean
