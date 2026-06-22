68eca58970734e52e800f2af7c6d966e1b0ed8b0
rproner1 <robert.proner.education@gmail.com>
2026-06-22 14:51:10 -0400

    06-22 dmq run

--- scripts/main_train.sh
+++ scripts/main_train.sh
@@ -5,7 +5,7 @@
 #SBATCH --account=rrg-camera
 #SBATCH --cpus-per-task=4
 #SBATCH --time=03-00:00
-#SBATCH --mem=240G
+#SBATCH --mem=150G
 #SBATCH --array=1997-2023
 #SBATCH --output=output/slurm-%A_%a.out
 #SBATCH --mail-user=robert.proner@mail.utoronto.ca
@@ -15,11 +15,8 @@ module load python/3.12
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 python -m pip install --no-index --upgrade pip
-#python -m pip install scikit-learn==1.5.0 statsmodels==0.14.5 tensorflow==2.17.0 quantile-forest==1.4.1 optuna==3.6.1 --no-index
-#python -m pip install numpy==1.26.4 pandas==2.2.3 portalocker==2.10.1 scipy==1.13.1 pyyaml==6.0.2 python-dotenv==1.2.1 --no-index
 python -m pip install --no-index -r requirements.txt
-#python -m pip install --no-index ~/projects/rrg-camera/rproner/xlstm
 
 TARGET=0
-DATE=2026-06-16
+DATE=2026-06-22
 python main_train.py --year $SLURM_ARRAY_TASK_ID --date $DATE --target $TARGET --model-type deep
