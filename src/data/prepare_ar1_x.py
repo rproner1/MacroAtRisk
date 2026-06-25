aed089ff8ee582432e7fc6afa9366ec30643fa35
Robert Proner <72316801+rproner1@users.noreply.github.com>
2026-06-25 16:10:58 -0400

    make AR features

--- src/data/prepare_ar1_x.py
+++ src/data/prepare_ar1_x.py
@@ -42,3 +42,59 @@ def get_ar1_x(
     ar1_features = ar1_features.loc[desired_start_date_of_samples:last_date_of_sample]
 
     return ar1_features
+
+def get_ar_x(
+        file_path: Path,
+        desired_start_date_of_samples: pd.Timestamp,
+        horizon_in_quarters: int,
+        last_date_of_sample: pd.Timestamp,
+        target: str,
+        lags: int = 12
+    ) -> pd.DataFrame:
+    """
+    Prepares AR(1) features from FRED target variables for modeling.
+
+    Parameters:
+        fred_file_name (str): Name of the FRED data CSV file.
+
+    Returns:
+        pd.DataFrame: Prepared AR(1) features ready for modeling.
+    """
+    df = pd.read_csv(file_path)
+    
+    # Fix date format and set as index
+    df = df.loc[1:, :] # skip first row with transformation codes
+    df['sasdate'] = pd.to_datetime(df['sasdate'], format='%m/%d/%Y')
+    df.set_index('sasdate', inplace=True)
+    df.index.name = 'date'
+
+    # Create target variables
+    if target == 'Infl_yoy':
+        t = np.log(df['CPIAUCSL']) - np.log(df['CPIAUCSL'].shift(12))
+    elif target == 'IP_yoy':
+        t = np.log(df['INDPRO']) - np.log(df['INDPRO'].shift(12))
+    elif target == 'Unrate_yoy':
+        t = np.log(df['UNRATE']) - np.log(df['UNRATE'].shift(12))
+    else:
+        raise ValueError('Not a valid target')
+    
+    target_df = pd.DataFrame({target: t})
+
+    lagged_features = []
+    for lag in range(lags):
+        x = target_df.shift(3*horizon_in_quarters + 1 + lag)
+        x.columns = [
+            f"{col}_t-{lag+1}" for col in target_df.columns
+        ]
+        lagged_features.append(x)
+
+    lagged_features = pd.concat(lagged_features, axis=1)
+
+    max_lag = 3*horizon_in_quarters + 1 + (lags-1)
+
+    lagged_features = lagged_features.loc[desired_start_date_of_samples:last_date_of_sample]
+
+    # Drop NaNs from lags
+    lagged_features = lagged_features.iloc[max_lag:]
+
+    return lagged_features
